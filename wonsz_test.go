62cf7f62b50bce5e665a5f162aae3406e47e3a90
Mrucznik <mrucznix@gmail.com>
2022-02-13 16:12:16 +0100

    add wonsz.BindConfig() example, reorganize tests

--- wonsz_test.go
+++ wonsz_test.go
@@ -1,20 +1,37 @@
 package wonsz
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
+	"os"
 	"testing"
 )
 
-type TestConfig struct {
-	AlwaysInConfig string `sometag:"xd" boolTag:""`
-	TestField      string `name:"SnakeName" booltag:"" kek01:"pur"`
-}
-
-var testConfig TestConfig
-
 func TestInitializeConfig(t *testing.T) {
+	var testConfig struct {
+		AlwaysInConfig string
+		TestField      string
+	}
+
 	err := BindConfig(&testConfig, &cobra.Command{}, ConfigOpts{})
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func ExampleBindConfig() {
+
+	os.Setenv("EXAMPLE_FIELD", "this is my example config field")
+
+	var myConfig struct {
+		ExampleField string
+	}
+
+	err := BindConfig(&myConfig, nil, ConfigOpts{})
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(myConfig.ExampleField)
+	// Output: this is my example config field
 }
