5118edd2ed5b66fe6fc4b3b118b5ccc6c3055080
Mrucznik <mrucznix@gmail.com>
2022-03-10 22:37:13 +0100

    add GetTagsForField test

--- reflection_test.go
+++ reflection_test.go
@@ -6,6 +6,12 @@ import (
 )
 
 func TestGetTagsForField(t *testing.T) {
+	type testStruct struct {
+		noTags   string
+		oneTag   string `someTag:"testTagValue"`
+		manyTags string `tag1:"value1" tag2:"value2" tag3:"value3"`
+	}
+
 	type args struct {
 		field reflect.StructField
 	}
@@ -14,7 +20,31 @@ func TestGetTagsForField(t *testing.T) {
 		args args
 		want []Tag
 	}{
-		// TODO: Add test cases.
+		{
+			name: "no tags",
+			args: args{
+				field: reflect.TypeOf(testStruct{}).Field(0),
+			},
+			want: nil,
+		},
+		{
+			name: "one tag",
+			args: args{
+				field: reflect.TypeOf(testStruct{}).Field(1),
+			},
+			want: []Tag{{name: "someTag", value: "testTagValue"}},
+		},
+		{
+			name: "many tags",
+			args: args{
+				field: reflect.TypeOf(testStruct{}).Field(2),
+			},
+			want: []Tag{
+				{name: "tag1", value: "value1"},
+				{name: "tag2", value: "value2"},
+				{name: "tag3", value: "value3"},
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
