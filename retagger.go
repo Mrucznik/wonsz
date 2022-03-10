0a85fe57e88bd398d45aa8b422a3c7ca130a9e79
Mrucznik <mrucznix@gmail.com>
2022-03-10 22:14:02 +0100

    fix make tag

--- retagger.go
+++ retagger.go
@@ -3,7 +3,6 @@ package wonsz
 import (
 	"fmt"
 	"reflect"
-	"strings"
 )
 
 // Structure for being used by retag.Convert to create desired mapstructure tags for config structure.
@@ -14,6 +13,12 @@ type mapstructureRetagger struct{}
 func (m mapstructureRetagger) MakeTag(structureType reflect.Type, fieldIndex int) reflect.StructTag {
 	field := structureType.Field(fieldIndex)
 	mapping := camelCaseToUnderscoredLowered(field.Name)
-	newTag := strings.Join([]string{fmt.Sprintf("mapstructure:\"%s\"", mapping), string(field.Tag)}, " ")
+
+	var newTag string
+	if field.Tag != "" {
+		newTag = fmt.Sprintf("mapstructure:\"%s\" %s", mapping, field.Tag)
+	} else {
+		newTag = fmt.Sprintf("mapstructure:\"%s\"", mapping)
+	}
 	return reflect.StructTag(newTag)
 }
