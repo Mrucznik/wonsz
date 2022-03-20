cdd9dd290a028502e8a9e6abe449f28e77cda76c
Mrucznik <mrucznix@gmail.com>
2022-03-20 17:55:26 +0100

    tests for binding slice

--- wonsz_test.go
+++ wonsz_test.go
@@ -35,3 +35,28 @@ func ExampleBindConfig() {
 	fmt.Println(myConfig.ExampleField)
 	// Output: this is my example config field
 }
+
+func Test_BindConfig_withEnv(t *testing.T) {
+	os.Setenv("TEST_FIELD", "this is my example config field")
+	os.Setenv("SLICE_FIELD", "some,text,here")
+
+	var testConfig struct {
+		TestField  string
+		SliceField []string
+	}
+
+	err := BindConfig(&testConfig, nil, ConfigOpts{})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if testConfig.TestField != "this is my example config field" {
+		t.Errorf("Expected %s, got %s", "this is my example config field", testConfig.TestField)
+	}
+
+	if testConfig.SliceField[0] != "some" ||
+		testConfig.SliceField[1] != "text" ||
+		testConfig.SliceField[2] != "here" {
+		t.Errorf("Expected %s, got %s", "some", testConfig.SliceField[0])
+	}
+}
